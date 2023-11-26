aaf0d61db346d4822ad8e6c9a161d2a580f311e7
Kayk Mascarenhas <kaykdsg@gmail.com>
2023-11-26 13:49:12 -0300

    feat: info messages after success and fails

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 from usuarios.forms import LoginForms, CadastroForms
 
 
@@ -16,8 +16,10 @@ def login(request):
         usuario = auth.authenticate(request, username=nome, password=senha)
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f"{nome} logado com sucesso!")
             return redirect("index")
         else:
+            messages.error(request, "Erro ao realizar o login!")
             return redirect("login")
 
     return render(request, "usuarios/login.html", {"form": form})
@@ -30,6 +32,7 @@ def cadastro(request):
         form = CadastroForms(request.POST)
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "As senhas não são iguais!")
                 return redirect("cadastro")
 
             nome = form["nome_cadastro"].value()
@@ -37,12 +40,14 @@ def cadastro(request):
             senha = form["senha_1"].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuário já cadastrado!")
                 return redirect("cadastro")
 
             usuario = User.objects.create_user(
                 username=nome, email=email, password=senha
             )
             usuario.save()
+            messages.success(request, "Cadastro realizado com sucesso!")
             return redirect("login")
 
     return render(request, "usuarios/cadastro.html", {"form": form})
